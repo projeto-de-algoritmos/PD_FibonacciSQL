c06b839681ba2819b73e3e5655948790ea0f5053
Bruno Oliveira <brunooliveira.unb@gmail.com>
2023-12-11 04:46:21 -0300

    adicionando algoritmo

---
+++ script.js
@@ -0,0 +1,21 @@
+function fibonacci(n) {
+    const fibArray = [0, 1];
+    for (let i = 2; i <= n; i++) {
+        fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+    }
+    return fibArray[n];
+}
+
+function calcular() {
+    const indexInput = document.getElementById('index');
+    const resultParagraph = document.getElementById('result');
+
+    const id = parseInt(indexInput.value);
+
+    if (!isNaN(id) && id >= 0) {
+        const fibonacciResult = fibonacci(id);
+        resultParagraph.textContent = `Fibonacci(${id}) = ${fibonacciResult}`;
+    } else {
+        resultParagraph.textContent = 'Por favor, insira um número inteiro não negativo.';
+    }
+}
